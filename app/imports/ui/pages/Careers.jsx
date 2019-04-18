99faf1fe7cfa944c0f9f0cd8085d4d5082060b54
Japhet Ye <japhetye@hawaii.edu>
2019-04-17 21:59:59 -1000

    Revert "Interests Page Expirament 1"
    This reverts commit 70dd99c3c64fa33c2e849a9d1526411c41adfa9d.

--- app/imports/ui/pages/Careers.jsx
+++ app/imports/ui/pages/Careers.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '/imports/api/stuff/stuff';
 import { withTracker } from 'meteor/react-meteor-data';
-import { Grid } from 'semantic-ui-react';
+import { Menu } from 'semantic-ui-react';
 
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 class Careers extends React.Component {
@@ -10,25 +10,13 @@ class Careers extends React.Component {
   /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
     return (
-        <Grid columns={2} divided>
-            <Grid.Row>
-                <Grid.Column>
-                    Hello
-                </Grid.Column>
-                <Grid.Column>
-                    There
-                </Grid.Column>
-            </Grid.Row>
-
-            <Grid.Row>
-                <Grid.Column>
-                    General
-                </Grid.Column>
-                <Grid.Column>
-                    Kenobi!
-                </Grid.Column>
-            </Grid.Row>
-        </Grid>
+       <div>
+           <Menu inverted widths={3}>
+               <Menu.Item activeClassName="active">Test1</Menu.Item>
+               <Menu.Item activeClassName="active">Test1</Menu.Item>
+               <Menu.Item activeClassName="active">Test1</Menu.Item>
+           </Menu>
+       </div>
     );
   }
 
