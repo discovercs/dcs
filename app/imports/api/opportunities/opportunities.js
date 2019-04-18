1b8e6568fd9a969f6737b80f28695e900513b09a
Brandon Wong <bwong7@hawaii.edu>
2019-04-17 22:47:04 -1000

    Added Opportunities Schema

---
+++ app/imports/api/opportunities/opportunities.js
@@ -0,0 +1,34 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+import { Tracker } from 'meteor/tracker';
+
+/** Create a Meteor collection. */
+const Opportunities = new Mongo.Collection('Opportunities');
+
+/** Create a schema to constrain the structure of documents associated with this collection. */
+const OpportunitiesSchema = new SimpleSchema({
+  id: String,
+  name: String,
+  description: String,
+  interestIDs: {
+    type: String,
+    array: []
+  },
+  careerIDs: {
+    type: String,
+    array: []
+  },
+  types: {
+    type: String,
+    array: []
+  },
+  date: Date,
+  year: Number,
+  picture: String,
+}, { tracker: Tracker });
+
+/** Attach this schema to the collection. */
+Opportunities.attachSchema(OpportunitiesSchema);
+
+/** Make the collection and schema available to other code. */
+export { Opportunities, OpportunitiesSchema };
