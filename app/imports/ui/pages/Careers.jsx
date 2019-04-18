bd7c16d359281c81efc07657eaaa9d3d085c03f7
Japhet Ye <japhetye@hawaii.edu>
2019-04-17 19:25:31 -1000

    God Help Us

--- app/imports/ui/pages/Careers.jsx
+++ app/imports/ui/pages/Careers.jsx
@@ -1,54 +1,25 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Container, Table, Header, Loader } from 'semantic-ui-react';
 import { Stuffs } from '/imports/api/stuff/stuff';
-import StuffItem from '/imports/ui/components/StuffItem';
 import { withTracker } from 'meteor/react-meteor-data';
-import PropTypes from 'prop-types';
+import { Menu } from 'semantic-ui-react';
 
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
-class ListStuff extends React.Component {
+class Careers extends React.Component {
 
   /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
-    return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
-  }
-
-  /** Render the page once subscriptions have been received. */
-  renderPage() {
     return (
-        <Container>
-          <Header as="h2" textAlign="center">List Stuff</Header>
-          <Table celled>
-            <Table.Header>
-              <Table.Row>
-                <Table.HeaderCell>Name</Table.HeaderCell>
-                <Table.HeaderCell>Quantity</Table.HeaderCell>
-                <Table.HeaderCell>Condition</Table.HeaderCell>
-                <Table.HeaderCell>Edit</Table.HeaderCell>
-              </Table.Row>
-            </Table.Header>
-            <Table.Body>
-              {this.props.stuffs.map((stuff) => <StuffItem key={stuff._id} stuff={stuff} />)}
-            </Table.Body>
-          </Table>
-        </Container>
+       <div>
+           <Menu inverted widths={3}>
+               <Menu.Item>Interest</Menu.Item>
+               <Menu.Item>Careers</Menu.Item>
+               <Menu.Item>Opportunities</Menu.Item>
+           </Menu>
+       </div>
     );
   }
+
 }
 
-/** Require an array of Stuff documents in the props. */
-ListStuff.propTypes = {
-  stuffs: PropTypes.array.isRequired,
-  ready: PropTypes.bool.isRequired,
-};
-
-/** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
-export default withTracker(() => {
-  // Get access to Stuff documents.
-  const subscription = Meteor.subscribe('Stuff');
-  return {
-    stuffs: Stuffs.find({}).fetch(),
-    ready: subscription.ready(),
-  };
-})(ListStuff);
+export default (Careers);
