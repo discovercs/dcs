70dd99c3c64fa33c2e849a9d1526411c41adfa9d
Japhet Ye <japhetye@hawaii.edu>
2019-04-17 21:53:51 -1000

    Interests Page Expirament 1

--- app/imports/ui/pages/Careers.jsx
+++ app/imports/ui/pages/Careers.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '/imports/api/stuff/stuff';
 import { withTracker } from 'meteor/react-meteor-data';
-import { Menu } from 'semantic-ui-react';
+import { Grid } from 'semantic-ui-react';
 
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 class Careers extends React.Component {
@@ -10,13 +10,25 @@ class Careers extends React.Component {
   /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
     return (
-       <div>
-           <Menu inverted widths={3}>
-               <Menu.Item activeClassName="active">Test1</Menu.Item>
-               <Menu.Item activeClassName="active">Test1</Menu.Item>
-               <Menu.Item activeClassName="active">Test1</Menu.Item>
-           </Menu>
-       </div>
+        <Grid columns={2} divided>
+            <Grid.Row>
+                <Grid.Column>
+                    Hello
+                </Grid.Column>
+                <Grid.Column>
+                    There
+                </Grid.Column>
+            </Grid.Row>
+
+            <Grid.Row>
+                <Grid.Column>
+                    General
+                </Grid.Column>
+                <Grid.Column>
+                    Kenobi!
+                </Grid.Column>
+            </Grid.Row>
+        </Grid>
     );
   }
 
--- app/imports/ui/pages/Interests.jsx
+++ app/imports/ui/pages/Interests.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '/imports/api/stuff/stuff';
 import { withTracker } from 'meteor/react-meteor-data';
-import { Menu } from 'semantic-ui-react';
+import {Grid, List, Segment} from 'semantic-ui-react';
 
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 class Interests extends React.Component {
@@ -10,13 +10,57 @@ class Interests extends React.Component {
   /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
     return (
-       <div>
-           <Menu inverted widths={3}>
-               <Menu.Item activeClassName="active">Test2</Menu.Item>
-               <Menu.Item activeClassName="active">Test2</Menu.Item>
-               <Menu.Item activeClassName="active">Test2</Menu.Item>
-           </Menu>
-       </div>
+        <Grid columns={2} divided>
+            <Grid.Row>
+                <Segment>
+                <Grid.Column>
+                    Languages
+                    <List as='ul'>
+                        <List.Item as='li'>C</List.Item>
+                        <List.Item as='li'>C++</List.Item>
+                        <List.Item as='li'>Java</List.Item>
+                        <List.Item as='li'>Javascript</List.Item>
+                    </List>
+                </Grid.Column>
+                </Segment>
+                <Segment>
+                <Grid.Column>
+                    Subjects
+                    <List as='ul'>
+                        <List.Item as='li'>Art</List.Item>
+                        <List.Item as='li'>Biology</List.Item>
+                        <List.Item as='li'>Astronomy</List.Item>
+                        <List.Item as='li'>Social Networking</List.Item>
+                    </List>
+                </Grid.Column>
+                </Segment>
+            </Grid.Row>
+
+            <Grid.Row>
+                <Segment>
+                <Grid.Column>
+                    Computer Science Fields
+                    <List as='ul'>
+                        <List.Item as='li'>Computer Science</List.Item>
+                        <List.Item as='li'>Data Science</List.Item>
+                        <List.Item as='li'>Compiler Designer</List.Item>
+                        <List.Item as='li'>Computer Networking</List.Item>
+                    </List>
+                </Grid.Column>
+                </Segment>
+                <Segment>
+                <Grid.Column>
+                    Personal
+                    <List as='ul'>
+                        <List.Item as='li'>Team Collaboration</List.Item>
+                        <List.Item as='li'>Leadership</List.Item>
+                        <List.Item as='li'>Mentoring</List.Item>
+                        <List.Item as='li'>Analysis</List.Item>
+                    </List>
+                </Grid.Column>
+                </Segment>
+            </Grid.Row>
+        </Grid>
     );
   }
 
