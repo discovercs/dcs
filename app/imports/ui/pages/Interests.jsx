b92505a63db6f690e193ae79cb698d4ef481c03d
Bela <mirabela@hawaii.edu>
2019-04-25 23:08:41 -1000

    update interests

--- app/imports/ui/pages/Interests.jsx
+++ app/imports/ui/pages/Interests.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '/imports/api/stuff/stuff';
 import { withTracker } from 'meteor/react-meteor-data';
-import { Segment, List } from 'semantic-ui-react';
+import { Segment, List, Button } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
@@ -13,43 +13,40 @@ class Interests extends React.Component {
     return (
         <Segment.Group>
             <Segment.Group horizontal>
-               <Link to='/home'> <Segment stretched>Languages
+               <Segment stretched><Link to='/home'><Button content="Languages" /></Link>
                 <List as='ol'>
                     <List.Item as='li' value='-'>C</List.Item>
                     <List.Item as='li' value='-'>C++</List.Item>
                     <List.Item as='li' value='-'>Java</List.Item>
                     <List.Item as='li' value='-'>JavaScript</List.Item>
                 </List>
-               </Segment> </Link>
-                <Link to='/home'>
-                <Segment stretched>Subjects
+               </Segment>
+                <Segment stretched><Link to='/home'><Button content="Subjects" /></Link>
                     <List as='ol'>
                         <List.Item as='li' value='-'>Biology</List.Item>
                         <List.Item as='li' value='-'>Art</List.Item>
                         <List.Item as='li' value='-'>Chemistry</List.Item>
                         <List.Item as='li' value='-'>Astronomy</List.Item>
                     </List>
-                </Segment> </Link>
+                </Segment>
             </Segment.Group>
             <Segment.Group horizontal>
-                <Link to='/home'>
-                <Segment stretched>Computer Science Fields
+                <Segment stretched><Link to='/home'><Button content="Fields" /></Link>
                     <List as='ol'>
                         <List.Item as='li' value='-'>Data Science</List.Item>
                         <List.Item as='li' value='-'>Computer Security</List.Item>
                         <List.Item as='li' value='-'>Algorithm Analysis</List.Item>
                         <List.Item as='li' value='-'>Web Development</List.Item>
                     </List>
-                </Segment> </Link>
-                <Link to='/home'>
-                <Segment stretched>Personal
+                </Segment>
+                <Segment stretched><Link to='/home'><Button content="Personal" /></Link>
                     <List as='ol'>
                         <List.Item as='li' value='-'>Team Collaborator</List.Item>
                         <List.Item as='li' value='-'>Leadership Roles</List.Item>
                         <List.Item as='li' value='-'>Community Builder</List.Item>
                         <List.Item as='li' value='-'>Innovator</List.Item>
                     </List>
-                </Segment> </Link>
+                </Segment>
             </Segment.Group>
         </Segment.Group>
     );
