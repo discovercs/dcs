66eef1638acda45b8ed6440aa627323d1b0a8348
Brandon Wong <bwong7@hawaii.edu>
2019-04-25 16:31:06 -1000

    Completed Friends Mockup

--- app/imports/ui/pages/Friends.jsx
+++ app/imports/ui/pages/Friends.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '/imports/api/stuff/stuff';
 import { withTracker } from 'meteor/react-meteor-data';
-import { Card, Container, Grid, Header, Image } from 'semantic-ui-react';
+import { Card, Container, Grid, Header, Image, Input, Button, Icon, Dropdown, List } from 'semantic-ui-react';
 
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 class Careers extends React.Component {
@@ -14,6 +14,35 @@ class Careers extends React.Component {
           <Grid style={{ margin: '50px' }} verticalAlign='middle' textAlign='center' columns = {3} container>
             <Grid.Column>
               <Header as='h1' >Find Friends</Header>
+
+              <List>
+                <List.Item><Input placeholder="Search..."/>
+                <Button><Icon name='search'/></Button></List.Item>
+
+                <List.Item>
+                  <Dropdown item text="Class Standing" icon="dropdown" as='h3'>
+                    <Dropdown.Menu>
+                      <Dropdown.Item>Freshman</Dropdown.Item>
+                      <Dropdown.Item>Sophomore</Dropdown.Item>
+                      <Dropdown.Item>Junior</Dropdown.Item>
+                      <Dropdown.Item>Senior</Dropdown.Item>
+                      <Dropdown.Item>Super-Senior</Dropdown.Item>
+                    </Dropdown.Menu>
+                  </Dropdown>
+                </List.Item>
+
+                <List.Item>
+                  <Dropdown item text="Similar Interests" icon="dropdown" as='h3'>
+                    <Dropdown.Menu>
+                      <Dropdown.Item>HTML</Dropdown.Item>
+                      <Dropdown.Item>CSS</Dropdown.Item>
+                      <Dropdown.Item>Javascript</Dropdown.Item>
+                      <Dropdown.Item>C/C++</Dropdown.Item>
+                      <Dropdown.Item>Python</Dropdown.Item>
+                    </Dropdown.Menu>
+                  </Dropdown>
+                </List.Item>
+              </List>
             </Grid.Column>
 
             <Grid.Column>
