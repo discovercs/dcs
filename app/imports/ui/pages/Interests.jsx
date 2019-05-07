99bd54e9be7bb52868ad7c860576018c9ba7f0c1
Brandon Wong <bwong7@hawaii.edu>
2019-05-06 20:51:35 -1000

    Added hero image

--- app/imports/ui/pages/Interests.jsx
+++ app/imports/ui/pages/Interests.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '/imports/api/stuff/stuff';
 import { withTracker } from 'meteor/react-meteor-data';
-import { Grid, Button, Header, Divider } from 'semantic-ui-react';
+import { Grid, Button, Header, Image } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
@@ -12,19 +12,19 @@ class Interests extends React.Component {
   render() {
     return (
         <div>
-          <Divider/>
-        <Header as='h1' textAlign="center">Find Interests</Header>
-        <Grid verticalAlign='middle' textAlign='center' columns = {3}>
-          <Grid.Column>
-            <Link to='/addlanguages'><Button content="Technologies" /></Link>
-          </Grid.Column>
-          <Grid.Column>
-            <Link to='/addsubjects'><Button content="Subjects" /></Link>
-          </Grid.Column>
-          <Grid.Column>
-            <Link to='/addfields'><Button content="Fields" /></Link>
-          </Grid.Column>
-        </Grid>
+          <Image centered fluid src="https://advising.utah.edu/_images/discover-your-interests-1030x300-cropped.jpg"/>
+          <Header as='h1' textAlign="center">Find Interests</Header>
+          <Grid verticalAlign='middle' textAlign='center' columns = {3}>
+            <Grid.Column>
+              <Link to='/addlanguages'><Button content="Technologies" /></Link>
+            </Grid.Column>
+            <Grid.Column>
+              <Link to='/addsubjects'><Button content="Subjects" /></Link>
+            </Grid.Column>
+            <Grid.Column>
+              <Link to='/addfields'><Button content="Fields" /></Link>
+            </Grid.Column>
+          </Grid>
         </div>
     );
   }
